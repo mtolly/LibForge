6763bbe1e323256e1ffc7f8153068090e63fe309
Michael Tolly <miketolly@gmail.com>
2024-02-20 20:40:28 -0600

    Support texture magic 3

--- LibForge/LibForge/Texture/TextureReader.cs
+++ LibForge/LibForge/Texture/TextureReader.cs
@@ -18,12 +18,14 @@ namespace LibForge.Texture
     public override Texture Read()
     {
       var magic = Int();
-      if(magic != 6 && magic != 4)
+      if(magic != 6 && magic != 4 && magic != 3)
       {
         throw new Exception($"Unknown texture magic {magic}");
       }
       var version = Int();
-      var hdrData = magic == 6 ? FixedArr(Byte, version == 0xC ? 0x7Cu : 0xACu) : FixedArr(Byte, 0xA4);
+      var hdrData = magic == 6 ? FixedArr(Byte, version == 0xC ? 0x7Cu : 0xACu)
+        : magic == 3 ? FixedArr(Byte, 0x8Cu)
+        : FixedArr(Byte, 0xA4u);
       var MipmapLevels = UInt();
       var Mipmaps = FixedArr(() => new Texture.Mipmap
       {
@@ -34,7 +36,14 @@ namespace LibForge.Texture
       UInt();
       for(var i = 0; i < Mipmaps.Length; i++)
       {
-        Mipmaps[i].Data = Arr(Byte);
+        if (magic == 3)
+        {
+          Mipmaps[i].Data = FixedArr(Byte, (uint) (Mipmaps[i].Width * Mipmaps[i].Height) / 2);
+        }
+        else
+        {
+          Mipmaps[i].Data = Arr(Byte);
+        }
       }
       var footerData = FixedArr(Byte, 0x1C);
       return new Texture
