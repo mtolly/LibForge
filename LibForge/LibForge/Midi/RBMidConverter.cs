553d4771fa30f876ce38f2f1c6245dee3e4e4587
charlotte <charlottemackenzi@gmail.com>
2024-08-31 20:05:17 +1200

    Fix bug with lyrics with no text

--- LibForge/LibForge/Midi/RBMidConverter.cs
+++ LibForge/LibForge/Midi/RBMidConverter.cs
@@ -1164,14 +1164,17 @@ namespace LibForge.Midi
         // Event handlers
         bool AddLyric(MidiText e)
         {
-          if (e.Text[0] != '[')
+          if (e.Text != "")
           {
-            lyrics.Add(new RBMid.TICKTEXT
+            if (e.Text[0] != '[')
             {
-              Text = e.Text.Trim(' '),
-              Tick = e.StartTicks,
-            });
-            return true;
+              lyrics.Add(new RBMid.TICKTEXT
+              {
+                Text = e.Text.Trim(' '),
+                Tick = e.StartTicks,
+              });
+              return true;
+            }
           }
           return false;
         }
